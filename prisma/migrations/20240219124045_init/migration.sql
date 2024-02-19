05b3aed1dc1ee69a001e2a2dc874df2404e6e737
Jeremy Warren <jeremyawarren15@gmail.com>
2024-02-19 12:47:55 +0000

    added basic model for troop

---
+++ prisma/migrations/20240219124045_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Troop" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Troop_pkey" PRIMARY KEY ("id")
+);
