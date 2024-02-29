8e423b27ab79c6e75f8e82617dd59413da720b73
Jeremy Warren <jeremyawarren15@gmail.com>
2024-02-29 06:42:09 +0000

    fixed event dates

---
+++ prisma/migrations/20240229064146_fixed_start_and_end_dates_for_event/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `enddate` on the `Event` table. All the data in the column will be lost.
+  - You are about to drop the column `startdate` on the `Event` table. All the data in the column will be lost.
+  - Added the required column `endDate` to the `Event` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `startDate` to the `Event` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Event" DROP COLUMN "enddate",
+DROP COLUMN "startdate",
+ADD COLUMN     "endDate" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "startDate" TIMESTAMP(3) NOT NULL;
